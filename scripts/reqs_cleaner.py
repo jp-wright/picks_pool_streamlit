8824a059c9370ab9af62b0519477e1b90d1e25e7
Jonpaul Wright <jonpaul.wright@gmail.com>
2021-11-21 14:56:41 -0600

    reqs

--- scripts/reqs_cleaner.py
+++ scripts/reqs_cleaner.py
@@ -16,3 +16,8 @@ with open("../requirements.txt", "w+"") as f:
     dd = df[df.columns[0]].str.extract('(.*\=.*)(\=\D.*)')[0].str.replace('=', '==').dropna().reset_index(drop=True)
     dd.rename(dd.iloc[0]).drop(dd.index[0]).to_csv('/Users/jpw/Dropbox/Data_Science/projects/github_projects/2021-11-10_nfl_picks_pool_streamlit/requirements.txt', index=False)
     
+    
+    
+    
+    df.loc[-1] = df.columns[0]
+    df = df.rename('temp').sort_index().reset_index(drop=True)
