4b3f3abda8a22fa9f8b9c9c6fbdaac809b328abb
Jonpaul Wright <jonpaul.wright@gmail.com>
2021-11-21 14:34:48 -0600

    update reqs.txt

--- scripts/reqs_cleaner.py
+++ scripts/reqs_cleaner.py
@@ -9,5 +9,10 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__))))
 p = "/Users/jpw/Dropbox/Data_Science/projects/github_projects/2021-11-10_nfl_picks_pool_streamlit/env/requirements_conda_condensed.txt"
 # with open("../requirements.txt", "w+"") as f:
-# with open("../requirements.txt", "w+"") as f:
+with open("../requirements.txt", "w+"") as f:
+    
+    
+    
+    dd = df[df.columns[0]].str.extract('(.*\=.*)(\=\D.*)')[0].str.replace('=', '==').dropna().reset_index(drop=True)
+    dd.rename(dd.iloc[0]).drop(dd.index[0]).to_csv('/Users/jpw/Dropbox/Data_Science/projects/github_projects/2021-11-10_nfl_picks_pool_streamlit/requirements.txt', index=False)
     
